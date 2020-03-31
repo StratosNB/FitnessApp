2cbc6b85548aba1b556f5d0f70ac0b734fc97f67
Bryan Romero <bryan_4764@hotmail.es>
2020-03-31 07:00:52 +0200

    Added custom Entry

---
+++ FitnessApp/FitnessApp.Android/CustomEntryRenderer.cs
@@ -0,0 +1,21 @@
+﻿using Android.Content;
+using FitnessApp.Droid;
+using FitnessApp;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(TransparentEntry), typeof(CustomEntryRenderer))]
+namespace FitnessApp.Droid
+{
+    public class CustomEntryRenderer : EntryRenderer
+    {
+        public CustomEntryRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            Control.SetBackgroundColor(Android.Graphics.Color.Transparent);
+        }
+    }
+}
