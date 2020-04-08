0b6e8703d2f1d76b40441ac65099e64b7d1af698
Bryan Romero <bryan_4764@hotmail.es>
2020-04-08 11:16:45 +0200

    Modified Namespaces

--- FitnessApp/FitnessApp/Controls/CustomStepper.cs
+++ FitnessApp/FitnessApp/Controls/CustomStepper.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Xamarin.Forms;
 
-namespace FitnessApp.Custom_Components
+namespace FitnessApp
 {
     public class CustomStepper : StackLayout
     {
